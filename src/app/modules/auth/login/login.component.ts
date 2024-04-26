d18c1a87f79564099e726ddbfffc4897b023dd5b
Ridmi211 <ridmiyatigammana@gmail.com>
2024-04-26 14:51:27 +0530

    Remove comments

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -14,31 +14,27 @@ export class LoginComponent implements OnInit {
   otpMode: boolean = false;
   username: string = '';
   errorMessage: string = '';
-  password:string='';
+  password: string = '';
 
   constructor(
     private loginService: LoginService,
     private userAuthService: UserAuthService,
     private router: Router,
     private toastr: ToastrService
-  ) {}
+  ) { }
 
-  ngOnInit(): void {}
-  
+  ngOnInit(): void { }
 
   onLogin(loginForm: NgForm) {
-    
     this.loginService.requestOTP(loginForm.value).subscribe(
       (response: any) => {
         console.log('OTP request response:', response);
         if (response.otpSent) {
-          // OTP sent successfully, switch to OTP mode
           this.otpMode = true;
           this.username = loginForm.value.username;
           this.password = loginForm.value.password;
         } else {
-          // Handle case where OTP sending failed
-          console.log(response.message); // Optional: Log additional message from the server
+          console.log(response.message);
         }
       },
       (error) => {
@@ -48,27 +44,23 @@ export class LoginComponent implements OnInit {
     );
   }
 
-  goBackLogin() {    
-          this.otpMode = false;      
+  goBackLogin() {
+    this.otpMode = false;
   }
-  
+
   onVerifyOTP(otpForm: NgForm) {
     const otp = otpForm.value.otp;
-    const otpData = { username: this.username, otp , password:this.password};
+    const otpData = { username: this.username, otp, password: this.password };
 
     this.loginService.verifyOTP(otpData).subscribe(
       (response: any) => {
-        // if (response.success) {
         console.log('OTP verification response:', response);
-        // Handle successful OTP verification (e.g., store token, navigate to dashboard)
         console.log('login response-----:', response);
         this.userAuthService.setRoles(response.roles);
         this.userAuthService.setToken(response.token);
         this.userAuthService.setUsername(response.username);
         this.userAuthService.setEmail(response.email);
-
         const role = response.roles[0];
-
         if (role === 'ROLE_SUPER_ADMIN') {
           this.router.navigate(['/super-admin-dashboard']);
         } else if (role === 'ROLE_ADMIN') {
@@ -76,18 +68,10 @@ export class LoginComponent implements OnInit {
         } else {
           this.router.navigate(['/login']);
         }
-      // }
-      // else {
-      //     console.log('Error verifying OTP:', response.message);
-      //     // Set the error message to be displayed to the user
-      //     this.errorMessage = response.message;
-      //     this.toastr.error(response.message, 'Error');
-      //   }
       },
       (error) => {
         console.log('Error verifying OTP:', error);
-         this.toastr.error('An error occurred during OTP verification. Please try again.', 'Error');
-        // Handle OTP verification error
+        this.toastr.error('An error occurred during OTP verification. Please try again.', 'Error');
       }
     );
   }
